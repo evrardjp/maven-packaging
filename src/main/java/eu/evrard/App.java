54cbf5425ec7abc8400eeccd9d5e420dd5f7b982
Jean-Philippe Evrard <jean-philippe@evrard.me>
2020-10-05 16:41:15 +0200

    Test

---
+++ src/main/java/eu/evrard/App.java
@@ -0,0 +1,13 @@
+package eu.evrard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
